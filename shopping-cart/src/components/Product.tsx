25013c3a9b97628954b577cd0ef972caa227ad1e
samsam.ali <samsam.muhumed.ali@accenture.com>
2026-05-22 11:02:27 +0100

    Tried fixing linetotal and subtotal

--- shopping-cart/src/components/Product.tsx
+++ shopping-cart/src/components/Product.tsx
@@ -2,26 +2,34 @@ import { useState } from 'react';
 import { Quantity } from './Quantity';
 
 type Props = {
+    //add name
+    id: number
     image: string,
     price: number,
 };
 
-export const Product = (props: Props) => {
-    const [quantity, setQuantity] = useState(0);
+type LineTotalProps = {
+    onChange: (id: number, lineTotal: number) => void;
+};
 
+type ProductProps = Props & LineTotalProps;
+
+export const Product = (productProps: ProductProps) => {
+    const [quantity, setQuantity] = useState(0);
     const handleQuantityChange = (qty: number) => {
         setQuantity(qty); 
     };
 
-    let subtotal = (quantity * props.price).toFixed(2);
+    let lineTotal = quantity * productProps.price;
+    productProps.onChange(productProps.id, lineTotal);
 
     return (
         <tr>
-            <td><img src={props.image} alt="wireless headphones" width="80" height="80"/></td>
+            <td><img src={productProps.image} alt="wireless headphones" width="80" height="80"/></td>
             <td>Wireless Headphones</td>
             <td><Quantity onChange={handleQuantityChange}/></td>
-            <td>£{props.price}</td>
-            <td>£{subtotal}</td>
+            <td>£{productProps.price}</td>
+            <td>£{lineTotal.toFixed(2)}</td>
         </tr>
     )
 }
--- shopping-cart/src/components/ProductsTable.tsx
+++ shopping-cart/src/components/ProductsTable.tsx
@@ -2,8 +2,31 @@ import wireless from './../assets/wireless.jpg';
 import cable from './../assets/cable.jpg';
 import stand from './../assets/stand.jpg';
 import { Product } from './Product';
+import { useState } from 'react';
 
 export const ProductsTable = () => {
+    let id = 0;
+
+    // const lineTotals = [0,0,0];
+    const [lineTotals, setLineTotals] = useState<number[]>([]);
+    console.log(lineTotals);
+
+    const handleLineTotalChange = (id: number, lineTotal: number) => {
+        // setLineTotals([1,2,3]);
+        // console.log("Line total: " + lineTotals);
+        setLineTotals((prev) => {
+            const updated = [...prev];
+            updated[id] = lineTotal;
+            return updated;
+        });
+    };
+    
+    const subtotal = lineTotals.reduce(
+        (sum, lineTotal) => sum + (lineTotal || 0),
+        0
+    );
+    
+
     return (
         <>
             <table>
@@ -15,11 +38,14 @@ export const ProductsTable = () => {
                         <th>Unit Price</th>
                         <th>Line Total</th>
                     </tr>
-                    <Product image={wireless} price={49.99}/>
-                    <Product image={cable} price={9.99}/>
-                    <Product image={stand} price={34.99}/>
+                    <Product id={id} image={wireless} price={49.99} onChange={() => handleLineTotalChange}/>
+                    <Product id={id++} image={cable} price={9.99} onChange={() => handleLineTotalChange}/>
+                    <Product id={id++} image={stand} price={34.99} onChange={() => handleLineTotalChange}/>
+                    
                 </tbody>
             </table>
+            <p>subtotal: {subtotal}</p>
         </>
     )
 }
+
